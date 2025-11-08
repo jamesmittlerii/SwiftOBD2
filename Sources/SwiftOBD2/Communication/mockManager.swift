d8c441747a35dff3eddb663453e566b8c07944b3
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-11-08 11:13:13 -0800

    add fuel status

--- Sources/SwiftOBD2/Communication/mockManager.swift
+++ Sources/SwiftOBD2/Communication/mockManager.swift
@@ -235,6 +235,8 @@ extension OBDCommand {
                     return "20 90 07 E0 11 00"
                 case .pidsC:
                     return "40 FA DC 80 00 00"
+             case .fuelStatus:
+                 return "03 02 04"
                 case .rpm:
                     let desiredRPM = Int.random(in: 1000...3000)
                     let decimalRep = desiredRPM * 4
